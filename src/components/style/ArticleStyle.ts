54b3f3da5ad5c4d7953d1cd94b676f0ff82427da
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-27 22:44:42 -0500

    wip: Paragraph style component

--- src/components/style/ArticleStyle.ts
+++ src/components/style/ArticleStyle.ts
@@ -14,6 +14,9 @@ export const Paragraph = styled.p`
   color: #979797;
   /* border: solid 1px red; */
   padding: 20px;
+  flex: 1;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  font-size: 14px;
   &:hover {
     opacity: 0.6;
   }
