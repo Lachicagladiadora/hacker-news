3d1419fe2f61e4b5f05a02c92867c3cb9bd06d67
Lachicagladiadora <200379@unsaac.edu.pe>
2024-01-07 23:02:28 -0500

    wip: render data from request

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ const URL = "https://hn.algolia.com/api/v1/search_by_date";
 const fetchData = async (param: string) => {
   const urlWithParams = param ? `${URL}?query=${param}` : URL;
   const data = await fetch(urlWithParams);
-  // .then((data) => data);
   console.log({ data });
   const dataValue = await data.json();
   console.log({ dataValue });
@@ -29,11 +28,57 @@ const fetchData = async (param: string) => {
 //   return error;
 // }
 
+type ArticleType = {
+  author: string;
+  comment_text: string;
+  created_at: string;
+  created_at_i: number;
+  objectID: string;
+  parent_id: number;
+  story_id: number;
+  story_title: string;
+  story_url: string;
+  updated_at: string;
+  _highlightResult: {
+    author: object;
+    comment_text: object;
+    story_title: object;
+    story_url: object;
+  };
+  _tags: string[];
+};
+
+type DataType = {
+  exhaustive: { nbHits: boolean; typo: boolean };
+  exhaustiveNbHits: boolean;
+  exhaustiveTypo: boolean;
+  hits: ArticleType[];
+  hitsPerPage: number;
+  nbHits: number;
+  nbPages: number;
+  page: number;
+  params: string;
+  processingTimeMS: number;
+  processingTimingsMS: {
+    afterFetch: number[] | object[];
+    fetch: number[];
+    _request: number[];
+  };
+  query: string;
+  serverTimeMS: number;
+};
+
 function App() {
   const [selectedValue, setSelectedValue] = useState<string>("react");
   const [visible, setVisible] = useState(false);
+  const [data, setData] = useState<DataType>();
 
-  const onSelectOption =
+  const onSelectOption = async (param: string) => {
+    setSelectedValue(param);
+    fetchData(param);
+    setData(await fetchData(param));
+  };
+
   return (
     <>
       <Header>Hacker News</Header>
@@ -55,35 +100,31 @@ function App() {
             <ContainOptions $visibility={visible ? "visible" : "hidden"}>
               <SelectorOption
                 children={"angular"}
-                onClick={() => {
-                  setSelectedValue("angular");
-                  fetchData("angular");
-                }}
+                onClick={() => onSelectOption("angular")}
               />
 
               <SelectorOption
                 children={"react"}
-                onClick={() => {
-                  setSelectedValue("react");
-                  fetchData("reactjs");
-                }}
+                onClick={() => onSelectOption("reactjs")}
               />
 
               <SelectorOption
                 children={"vue"}
-                onClick={() => setSelectedValue("vue")}
+                onClick={() => onSelectOption("vuejs")}
               />
             </ContainOptions>
           </Selector>
         </SectionForSelector>
         <SectionForArticles>
-          <Article>
-            <Paragraph
-              time="1 minute"
-              author="dardos"
-              text="Eating fewer calories can ward off ageing"
-            />
-          </Article>
+          {data?.hits.map((cur, idx) => (
+            <Article key={idx}>
+              <Paragraph
+                time={cur.created_at}
+                author={cur.author}
+                text={cur.story_title}
+              />
+            </Article>
+          ))}
         </SectionForArticles>
       </Main>
     </>
