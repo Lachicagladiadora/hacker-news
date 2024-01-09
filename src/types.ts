d6d0085641776829142cce4e9aa4aa179dde3abd
Lachicagladiadora <200379@unsaac.edu.pe>
2024-01-08 21:35:07 -0500

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+type ArticleType = {
+  author: string;
+  comment_text: string;
+  created_at: string;
+  created_at_i: number;
+  objectID: string;
+  parent_id: number;
+  story_id: number;
+  story_title: string;
+  story_url: string;
+  updated_at: string;
+  _highlightResult: {
+    author: object;
+    comment_text: object;
+    story_title: object;
+    story_url: object;
+  };
+  _tags: string[];
+};
+
+export type DataType = {
+  exhaustive: { nbHits: boolean; typo: boolean };
+  exhaustiveNbHits: boolean;
+  exhaustiveTypo: boolean;
+  hits: ArticleType[];
+  hitsPerPage: number;
+  nbHits: number;
+  nbPages: number;
+  page: number;
+  params: string;
+  processingTimeMS: number;
+  processingTimingsMS: {
+    afterFetch: number[] | object[];
+    fetch: number[];
+    _request: number[];
+  };
+  query: string;
+  serverTimeMS: number;
+};
