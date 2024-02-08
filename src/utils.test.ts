0ddb6d84bc9bc05d018b606d8c068d3f5ad9fe46
Lachicagladiadora <200379@unsaac.edu.pe>
2024-02-07 22:32:10 -0500

    test: unit test wit jest

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -8,7 +8,17 @@ describe("test utils", () => {
     );
   });
   // testear cuando tiene query y page
+  test("test add query and page params to the URL", () => {
+    const newUrl = addParamsToTheURL({ query: "angular", page: 1 });
+    expect(newUrl).toBe(
+      "https://hn.algolia.com/api/v1/search_by_date?query=angular&page=1"
+    );
+  });
   // testear si no se envia nada
+  test("test without params to the URL", () => {
+    const newUrl = addParamsToTheURL({ query: "" });
+    expect(newUrl).toBe("https://hn.algolia.com/api/v1/search_by_date?query=");
+  });
 
-  test("test get stories without page", () => {});
+  // test("test get stories without page", () => {});
 });
